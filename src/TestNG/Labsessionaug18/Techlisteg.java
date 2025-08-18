758cdf867fef6bab2159ac5ef01a6a31e475b27d
Pranavi1520 <127762994+Pranavi1520@users.noreply.github.com>
2025-08-18 17:49:40 +0530

    Add files via upload

---
+++ src/TestNG/Labsessionaug18/Techlisteg.java
@@ -0,0 +1,121 @@
+package com.wipro.testng.Labsessionaug18;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Techlisteg {
+	
+	
+@Test (groups = {"Sanity" , "Regression"})
+public void form()throws InterruptedException {
+		
+        ChromeOptions chromeoptions = new ChromeOptions();
+		
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver(chromeoptions);
+		
+		driver.get("https://www.techlistic.com/p/selenium-practice-form.html");
+		
+		driver.manage().window().maximize();
+		
+		Thread.sleep(2000);
+		
+        // Enter Firstname
+        
+        WebElement firstname = driver.findElement(By.xpath("//input[@name='firstname']"));
+        
+        firstname.sendKeys("Smith");
+        
+        Thread.sleep(2000);
+        
+        // Enter Lasttname
+        
+        WebElement lastname = driver.findElement(By.xpath("//input[@name='lastname']"));
+        
+        lastname.sendKeys("gold");
+        
+        Thread.sleep(2000);
+        
+        //Gender
+        
+        WebElement gender = driver.findElement(By.xpath("//input[@id='sex-0']"));
+		
+		gender.click();
+		
+		Thread.sleep(2000);
+	    
+		//years of experince
+		
+        WebElement exp = driver.findElement(By.xpath("//input[@id='exp-2']"));
+		
+		exp.click();
+		
+		Thread.sleep(2000);
+		
+		//Date
+		
+        WebElement date = driver.findElement(By.xpath("//input[@id='datepicker']"));
+		
+		date.sendKeys("17-09-2022");
+		
+		Thread.sleep(2000);
+		
+		//profession
+		
+        WebElement profession = driver.findElement(By.xpath("//input[@id='profession-1']"));
+		
+        profession.click();
+		
+		Thread.sleep(2000);
+		
+		//Tools
+		
+        WebElement tools = driver.findElement(By.xpath("//input[@id='tool-2']"));
+		
+        tools.click();
+		
+		Thread.sleep(2000);
+		
+		//continents
+		
+        WebElement conti = driver.findElement(By.xpath("//select[@id='continents']"));
+		
+		Select sel = new Select(conti);
+		
+		//select by visible text
+		
+		sel.selectByVisibleText("Asia");
+		
+		Thread.sleep(2000);
+		
+        //commands
+		
+        WebElement command = driver.findElement(By.xpath("//option[normalize-space()='Switch Commands']"));
+		
+		command.click();
+		
+		Thread.sleep(2000);
+		
+		// file upload
+		
+        WebElement fileupload = driver.findElement(By.xpath("//input[@id='photo']"));
+		
+		fileupload.sendKeys("C:\\Users\\pranavi\\Downloads\\image.png");
+		
+		Thread.sleep(2000);
+		
+		//submit
+		
+        WebElement Button = driver.findElement(By.xpath("//button[@id='submit']"));
+        
+        Button.click();
+}
+}
